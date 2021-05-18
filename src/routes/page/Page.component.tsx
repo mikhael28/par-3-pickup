26bc4fe6c156dc24bdb4e806559e373ce44fd513
mikhael28 <mikhael@hey.com>
2021-05-18 16:39:29 -0700

    Fixing build bug

--- src/routes/page/Page.component.tsx
+++ src/routes/page/Page.component.tsx
@@ -9,140 +9,120 @@ import GameNav from 'components/header/game-nav';
 import { usePageData, usePageDetails } from 'hooks/page';
 import { injectClassNames } from 'utils/css';
 import styles from './Page.module.scss';
-import { courses, Course, Game } from '../../config'
+import { courses, Course, Game } from '../../config';
 import { useRouter } from 'next/router';
 
-const {
-    page,
-    pageLanding,
-    pageContent,
-    placeholder
-} = styles;
+const { page, pageLanding, pageContent, placeholder } = styles;
 
 type PageProps = {
-    isLanding?: boolean
-};
-
-export const addTitleTags = (title: string): JSX.Element => {
-    if (!title) {
-        return <React.Fragment />;
-    }
-
-    return (
-        <>
-            <title>{ title }</title>
-            <meta name="og:title" content={ title } />
-        </>
-    );
-};
-
-export const addDescriptionTag = (description: string): JSX.Element => {
-    if (!description) {
-        return <></>;
-    }
-
-    return (
-        <meta
-          name="description"
-          property="og:description"
-          content={ description }
-        />
-    );
+	isLanding?: boolean;
 };
 
 export default function Page(props: PageProps): JSX.Element {
-    const { isLanding } = props;
-    const { title = '', description = '' } = usePageDetails();
-    const { content = '' } = usePageData();
-    const router = useRouter();
-    const [active, setActive] = useState<boolean>(true);
-    const [activeCourse, setActiveCourse] = useState<Course>({
-        id: '',
-        name: '',
-        codeName: '',
-        par3: false,
-        eighteen: false,
-        street: '',
-        city: '',
-        zip: '',
-        par: 0,
-        putting: 0,
-        picture: '',
-        holes: []
-    })
+	const { isLanding } = props;
+	const { title = '', description = '' } = usePageDetails();
+	const { content = '' } = usePageData();
+	const router = useRouter();
+	const [ active, setActive ] = useState<boolean>(false);
+	const [ activeCourse, setActiveCourse ] = useState<Course>({
+		id: '',
+		name: '',
+		codeName: '',
+		par3: false,
+		eighteen: false,
+		street: '',
+		city: '',
+		zip: '',
+		par: 0,
+		putting: 0,
+		picture: '',
+		holes: []
+	});
 
-    const [activeGame, setActiveGame] = useState<Game>({
-        id: '',
-        course: '',
-        perHoleWager: 0,
-        ldWager: 0,
-        players: [],
-        holes: []
-    })
+	const [ activeGame, setActiveGame ] = useState<Game>({
+		id: '',
+		course: '',
+		perHoleWager: 0,
+		ldWager: 0,
+		players: [],
+		holes: []
+	});
 
-    const classNames = injectClassNames(page, [pageLanding, isLanding]);
+	const classNames = injectClassNames(page, [ pageLanding, isLanding ]);
 
-    useEffect(() => {
-        // localStorage.clear()
-        let gameCheck = localStorage.getItem('activeCourse');
-        if (gameCheck !== null) {
-            console.log('Course Check: ', JSON.parse(gameCheck));
-            setActiveCourse(JSON.parse(gameCheck));
-        }
-        let activeCheck = localStorage.getItem('activeGame');
-        if (activeCheck !== null) {
-            console.log('Game check: ', JSON.parse(activeCheck))
-            setActiveGame(JSON.parse(activeCheck));
-            setActive(true);
-        }
-    }, [])
+	useEffect(() => {
+		// localStorage.clear()
+		let gameCheck = localStorage.getItem('activeCourse');
+		if (gameCheck !== null) {
+			console.log('Course Check: ', JSON.parse(gameCheck));
+			setActiveCourse(JSON.parse(gameCheck));
+		}
+		let activeCheck = localStorage.getItem('activeGame');
+		if (activeCheck !== null) {
+			console.log('Game check: ', JSON.parse(activeCheck));
+			setActiveGame(JSON.parse(activeCheck));
+			setActive(true);
+		}
+	}, []);
 
-    useEffect(() => {
-        console.log('New score')
-    }, [activeGame])
+	useEffect(
+		() => {
+			console.log('New score');
+		},
+		[ activeGame ]
+	);
 
-    function update(game) {
-        setActiveGame(game)
-    }
+	function update(game: any) {
+		setActiveGame(game);
+	}
 
-    return (
-        <>
-            <Head>
-                { addTitleTags(title) }
-                { addDescriptionTag(description) }
-                <meta name="robots" content="INDEX,FOLLOW" />
-            </Head>
-            <main>
-                {/* { isLanding && <Landing /> } */}
-                <section>
-                    
-                    <A2HS />
-                    {active === true ? (
-                        <GameNav activeGame={activeGame} activeCourse={activeCourse} update={update} />
-                        ) : <div>
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Golf</title>
+				<meta name="og:title" content="Seattle Pickup Golf" />
+				<meta
+					name="description"
+					property="og:description"
+					content="The beginners community for pickup golf in Seattle"
+				/>
+				<meta name="robots" content="INDEX,FOLLOW" />
+			</Head>
+			<main>
+				{/* { isLanding && <Landing /> } */}
+				<section>
+					<A2HS />
+					{active === true ? (
+						<GameNav activeGame={activeGame} activeCourse={activeCourse} update={update} />
+					) : (
+						<div>
+							<h1>Choose your course 🏌️‍♂️ ⛳</h1>
+							{courses.map((course, index) => {
+								return (
+									<Card
+										{...course}
+										handleClick={() => {
+											localStorage.setItem('activeCourse', JSON.stringify(course));
+											router.push(`/matchmaking`);
+										}}
+										key={index}
+									/>
+								);
+							})}
+						</div>
+					)}
 
-                    <h1>Choose your course 🏌️‍♂️ ⛳</h1>
-                    {courses.map((course, index) => {
-                        return(
-                            <Card {...course} handleClick={() => {
-                                localStorage.setItem('activeCourse', JSON.stringify(course));
-                                router.push(`/matchmaking`);
-                            }} key={index} />
-                        )
-                    })}
-                        </div>}
-                    
-                    {/* <Card title="Fore!" description="Click or tap here to start a new game!" onClick={() => props.history.push('/course/jackson')} /> */}
-                        {/* <div className={pageContent}>
+					{/* <Card title="Fore!" description="Click or tap here to start a new game!" onClick={() => props.history.push('/course/jackson')} /> */}
+					{/* <div className={pageContent}>
                             <>
                                 <figure className={ placeholder } />
                                 <figure className={ placeholder } />
                                 <figure className={ placeholder } />
                             </>
                         </div> */}
-                    {/* <Sidebar /> */}
-                </section>
-            </main>
-        </>
-    );
+					{/* <Sidebar /> */}
+				</section>
+			</main>
+		</React.Fragment>
+	);
 }
