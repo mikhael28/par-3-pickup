2bcfa6905c22bf560d2ccf272c2d595d03868402
mikhael28 <mikhael@hey.com>
2021-08-03 15:34:00 -0700

    Added sentry for testing drew's android device

--- next.config.js
+++ next.config.js
@@ -13,7 +13,22 @@ const serviceWorkerPath = 'static/sw.js';
 const serviceWorkerUrl = `/_next/${serviceWorkerPath}`;
 const serviceWorkerDest = `.next/${serviceWorkerPath}`;
 
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const SentryWebpackPluginOptions = {
+	// Additional config options for the Sentry Webpack plugin. Keep in mind that
+	// the following options are set automatically, and overriding them is not
+	// recommended:
+	//   release, url, org, project, authToken, configFile, stripPrefix,
+	//   urlPrefix, include, ignore
+  
+	silent: true, // Suppresses all logs
+	// For all available options, see:
+	// https://github.com/getsentry/sentry-webpack-plugin#options.
+  };
+
+
+const moduleExports = {
 	reactStrictMode: true,
 	env: {
 		serviceWorkerUrl
@@ -135,3 +150,6 @@ module.exports = {
 		}
 	]
 };
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+
