2968708de7035c7a68b02bfa14d4d17c3161200d
mikhael28 <mikhael@hey.com>
2021-08-12 16:04:39 -0700

    Testing service worker fix

--- src/sw/utils/getBuildManifest.ts
+++ src/sw/utils/getBuildManifest.ts
@@ -1,14 +1,15 @@
 declare const self: ServiceWorkerGlobalScope;
 
 const getBuildManifest = (): NextBuildManifest => {
-    const manifest = self.__BUILD_MANIFEST;
-
-    return Object.entries(manifest).reduce<NextBuildManifest>(
-        (manifest, [page, assets]) => ({
-            ...manifest,
-            [page]: assets.map(url => `/_next/${url}`)
-        }), {}
-    );
+  const manifest = self.__BUILD_MANIFEST;
+  console.log("manifest ts: ", manifest);
+  return Object.entries(manifest).reduce<NextBuildManifest>(
+    (manifest, [page, assets]) => ({
+      ...manifest,
+      //   [page]: assets.map((url) => `/_next/${url}`),
+    }),
+    {}
+  );
 };
 
 export default getBuildManifest;
